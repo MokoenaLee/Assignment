7be7aaddee472119e5073121e7b1534334f9a6a5
Palesa Mokoena <lee-annmokoena@Palesas-Air.hsd1.co.comcast.net>
2020-06-14 16:46:13 -0600

    put assignment requirements in methods

--- Assignment.rb
+++ Assignment.rb
@@ -1,7 +1,65 @@
-
-require 'rest-client'
+#!/usr/bin/env ruby
 
 #rest client documentation found at: https://github.com/rest-client/rest-client
+
+require 'rest-client' 
+
+class Assignment
+	#static method of the class
+	def self.get_course_details
+		get_response = RestClient.get('https://canvas.instructure.com/api/v1/courses/2106760', {:Authorization => 'Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq'})
+		puts "=======Course Details==========================="
+		puts "Response Code: #{get_response.code}"
+		puts get_response.body
+	end
+
+
+	def self.change_course_name(course_name)
+		put_response = RestClient::Request.execute(method: :put,
+                                             url: 'https://canvas.instructure.com/api/v1/courses/2106760',
+                                             payload: {"course[name]" => "#{course_name}"},
+                                             headers: {"Authorization" => "Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq"})
+		
+		puts "=======Course with Updated Name==========================="
+
+		puts " Updated course name is #{put_response.body}"
+	end
+
+
+
+	def self.get_course_settings
+		get_course_settings = RestClient.get('https://canvas.instructure.com/api/v1/courses/2106760/settings', {:Authorization => 'Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq'})
+		puts "========Course Settings========================================================"
+		puts "Course settings:  #{get_course_settings.body}"
+	end
+
+
+
+	def self.restrict_student_discussion
+		put_changeSettings = RestClient::Request.execute(method: :put,
+                                             url: 'https://canvas.instructure.com/api/v1/courses/2106760/settings',
+                                             payload: {"allow_student_discussion_topics" => false, "allow_student_discussion_editing" => true},
+                                             headers: {"Authorization" => "Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq"})
+		puts "=========Updated to not allow student_discussion==============="
+		
+		puts "changed settings #{put_changeSettings}"
+
+	end
+
+
+end
+
+
+Assignment.get_course_details
+
+Assignment.get_course_settings
+
+Assignment.change_course_name(ARGV[0])
+
+Assignment.restrict_student_discussion
+
+
+
 
 
 
@@ -14,14 +72,7 @@ added to this is the api doumentation format for a specific course
 #= begin 
 #fetch existing course
 
-get_response = RestClient.get('https://canvas.instructure.com/api/v1/courses/2106760', {:Authorization => 'Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq'})
 
-
-puts "Response Code: #{get_response.code}"
-
-#response of request returned in the body
-
-puts get_response.body
 
 
 
@@ -39,11 +90,7 @@ puts get_response.body
 =end 
 
 
-put_response = RestClient::Request.execute(method: :put,
-                                             url: 'https://canvas.instructure.com/api/v1/courses/2106760',
-                                             payload: {"course[name]" => "Creative Writing"},
-                                             headers: {"Authorization" => "Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq"})
-puts " Updated course name is #{put_response}"
+
 
 
 
@@ -53,21 +100,7 @@ puts " Updated course name is #{put_response}"
 
 =end 
 
-get_course_settings = RestClient.get('https://canvas.instructure.com/api/v1/courses/2106760/settings', {:Authorization => 'Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq'})
 
-puts "=============================================================================================================="
-
-puts "course settings #{get_course_settings.body}"
-
-
-puts "==================================editing settings changed======================================================"
-
-put_changeSettings = RestClient::Request.execute(method: :put,
-                                             url: 'https://canvas.instructure.com/api/v1/courses/2106760/settings',
-                                             payload: {"allow_student_discussion_topics" => false, "allow_student_discussion_editing" => true},
-                                             headers: {"Authorization" => "Bearer 7~Ic2qTMmcJF3ci6LCGvf4Iw04XYdvKjeokVZzvsHOtJ1ppSLtXDLPo7bvacNC8XVq"})
-
-puts "changed settings #{put_changeSettings}"
 
 
 
